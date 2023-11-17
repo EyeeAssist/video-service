f2e62893badc43a7b9b70e3af59a261bf1cf631c
XantX <hp_xantx@hotmail.com>
2023-11-17 17:10:17 -0500

    Process video and generate descripcion

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from auth.auth_service import api_key_auth
 from video.video_entity import Video
-from video.video_service import is_youtube_url, download_video
+from video.video_service import is_youtube_url, download_video, read_video
+from image.image_service import process_image
 
 
 app = FastAPI()
@@ -20,6 +21,14 @@ app.add_middleware(
 async def process_video(video: Video):
     if is_youtube_url(video.link):
         path = download_video(video.link)
-        return {"video": path}
+        video_frames = read_video(path)
+        result = []
+        for frame in video_frames:
+            print('Describing image in the second -> ', frame['time'])
+            description = process_image(frame['frame_image'])
+            print('Description ->', description)
+            result.append({'time': frame['time'],
+                           'description': description})
+        return {"video": result}
     else:
         raise HTTPException(status_code=400, detail="El link no es un link de youtube valido")
--- video/video_service.py
+++ video/video_service.py
@@ -1,6 +1,9 @@
 import pytube
 import re
 import uuid
+import cv2
+import numpy as np
+from PIL import Image
 
 
 def download_video(url: str):
@@ -36,3 +39,78 @@ def is_youtube_url(url):
     match = re.match(regex, url)
 
     return match is not None
+
+
+def calcular_similitud(img1, img2):
+    # Convierte las imágenes a espacios de color HSV
+    hsv_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
+    hsv_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+
+    # Calcula los histogramas 2D de las imágenes
+    hist_img1 = cv2.calcHist([hsv_img1], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    hist_img2 = cv2.calcHist([hsv_img2], [0, 1], None, [180, 256], [0, 180, 0, 256])
+
+    # Normaliza los histogramas
+    cv2.normalize(hist_img1, hist_img1, 0, 1, cv2.NORM_MINMAX)
+    cv2.normalize(hist_img2, hist_img2, 0, 1, cv2.NORM_MINMAX)
+
+    # Calcula la similitud entre los histogramas usando la distancia de Bhattacharyya
+    similitud = cv2.compareHist(hist_img1, hist_img2, cv2.HISTCMP_BHATTACHARYYA)
+
+    return similitud
+
+
+def es_frame_borrosa(frame, umbral_variacion_gradiente=100):
+    # Convierte el frame a escala de grises
+    frame_gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Calcula el gradiente usando el operador Sobel
+    gradiente_x = cv2.Sobel(frame_gris, cv2.CV_64F, 1, 0, ksize=5)
+    gradiente_y = cv2.Sobel(frame_gris, cv2.CV_64F, 0, 1, ksize=5)
+
+    # Calcula la magnitud del gradiente
+    magnitud_gradiente = np.sqrt(gradiente_x**2 + gradiente_y**2)
+
+    # Calcula la varianza de la magnitud del gradiente
+    varianza_gradiente = np.var(magnitud_gradiente)
+
+    # Compara la varianza con el umbral
+    return varianza_gradiente < umbral_variacion_gradiente
+
+def read_video(path):
+    video = cv2.VideoCapture(path)
+    if not video.isOpened():
+        print('Error al abrir video')
+        return None
+
+    frame_interval = 100
+
+    frames_list = []
+    current_time = 0.0
+    print('Leyendo video ->', path)
+    while True:
+        video.set(cv2.CAP_PROP_POS_MSEC, current_time)
+        ret, frame = video.read()
+        if not ret:
+            break
+        tiempo_actual = video.get(cv2.CAP_PROP_POS_MSEC)/1000
+        print('Leyendo tiempo -> ', tiempo_actual)
+        if current_time == 0.0:
+            imagen_pillow = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            frames_list.append({'time': tiempo_actual,
+                                'frame_image': imagen_pillow.convert("RGB"),
+                                'cv_image': frame})
+        elif current_time > 0.0 and frames_list[-1]:
+            similitud = calcular_similitud(frames_list[-1]['cv_image'], frame)
+            print('Similitud ->', similitud)
+            if similitud > 0.69 and not es_frame_borrosa(frame):
+                imagen_pillow = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                frames_list.append({'time': tiempo_actual,
+                                    'frame_image': imagen_pillow.convert("RGB"),
+                                    'cv_image': frame})
+
+        current_time += frame_interval
+
+    video.release()
+    cv2.destroyAllWindows()
+    return frames_list
